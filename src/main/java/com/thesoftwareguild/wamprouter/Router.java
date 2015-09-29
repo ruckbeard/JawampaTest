80693ce5a112883ef070a99b4e65c9b48a1f6c44
Kevin VanderWulp <kvwulp@gmail.com>
2015-09-29 19:58:16 -0400

    working test

--- src/main/java/com/thesoftwareguild/wamprouter/Router.java
+++ src/main/java/com/thesoftwareguild/wamprouter/Router.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.thesoftwareguild.wamprouter;
 
 import java.io.IOException;
@@ -62,6 +57,8 @@ public class Router {
             e.printStackTrace();
             return;
         }
+        
+        System.out.println("Router running");
         
         waitUntilKeypressed();
         System.out.println("Closing router");
